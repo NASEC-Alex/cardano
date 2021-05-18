71bc6c862f2f27bb20c3437bc10c620b93f89d5e
NASEC-Alex <80166471+NASEC-Alex@users.noreply.github.com>
2021-05-18 23:14:27 +0100

    Resolved incorrectly created protocol headers

--- handshake.py
+++ handshake.py
@@ -8,6 +8,7 @@ import cbor2
 import time
 import logging
 import socket
+
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 network_magic = 764824073
 
@@ -63,24 +64,34 @@ def parse_headers(resp):
     headers['mini_protocol'] = int(mode_mini_protcol[1:], 2)
     return headers
 
+def build_headers(protocol_id, payload, mode=0):
+    # Create the object for verison proposal
+    header = dict()
+    header['time'] = struct.pack('>I', int(time.monotonic() * 1000))
+    header['mode'] = mode.to_bytes(1, 'little')
+    header['mini_protocol_id'] = protocol_id.to_bytes(1, 'big')
+    header['length'] = len(payload).to_bytes(2, 'big')
+    logging.debug(header)
+    logging.debug('Request Time Binary: ' + convert_bits(header['time']))
+    logging.debug('Request Mode Binary: ' + convert_bits(header['mode']) + convert_bits(header['mini_protocol_id']))
+    logging.debug('Length: ' + convert_bits(header['length']))
+    return header
+
 def handshake(sock):
     '''
     Handshake with the Cardano Node
     '''
-    # Create the object for verison proposal
-    start_time = int(time.monotonic() * 1000)
     # You can propose all of the versions
     # obj = [0, {1 : network_magic, 2: network_magic, 3: network_magic, 4: [network_magic, False], 5: [network_magic, False], 6: [network_magic, False], 7: [network_magic, False]}]
     obj = [0, {4: [network_magic, False]}]
     # Object as CBOR
     cbor_obj = cbor2.dumps(obj)
-    # Time in milliseconds
-    cbor_time = struct.pack('>I', start_time)
+    header = build_headers(0, cbor_obj)
+    logging.debug(convert_bits(header['time'] + header['mode'] + header['mini_protocol_id'] + header['length']))
     # Length of payload
-    length = struct.pack('>I', len(cbor_obj))
-    msg = cbor_time + length + cbor_obj
-    logging.debug('Timestamp: ' + str(start_time) + ' ' + cbor2.dumps(cbor_obj).hex())
-    logging.debug('Length: ' + str(len(cbor_obj)) + ' ' + length.hex())
+    msg = header['time'] + header['mode'] + header['mini_protocol_id'] + header['length'] + cbor_obj
+    logging.debug('Timestamp: ' + str(header['time']) + ' ' + cbor2.dumps(cbor_obj).hex())
+    logging.debug('Length: ' + str(len(cbor_obj)) + ' ' + header['length'].hex())
     logging.debug('Version Options: ' + str(obj) + ' ' + cbor2.dumps(cbor_obj).hex())
     logging.debug('Constructed Payload: ' + msg.hex())
     # STATE: PROPOSE
